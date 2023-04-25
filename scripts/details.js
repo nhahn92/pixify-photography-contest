15df10a70c824431c423bc2e07232b70976c766b
Nicole Hahn <nicolehahn92@gmail.com>
2023-04-25 17:11:28 -0400

    Added code to populate page and toggle theme

---
+++ scripts/details.js
@@ -0,0 +1,111 @@
+const imageSets = [
+    {
+      id:1,  
+      name: 'Andrew Smith',
+      title: 'Innocence',
+      images: [
+        'https://images.unsplash.com/photo-1512540315028-2c1a6497da04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1543332143-4e8c27e3256f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1734&q=80',
+        'https://images.unsplash.com/photo-1612209246511-5b81949e5fdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+        'https://images.unsplash.com/photo-1492680967812-4b9226a079de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
+      ],
+    },
+    {
+      id:2,
+      name: 'Jane Doe',
+      title: 'Nature Adventures',
+      images: [
+        'https://images.unsplash.com/photo-1533692328991-08159ff19fca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80',
+        'https://images.unsplash.com/photo-1508515803898-7bb2d7703093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1503557122744-b650066ba62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1516298773066-c48f8e9bd92b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1598726668148-99946ef1cb42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80'
+      ],
+    },
+    {
+      id:3,
+      name: 'Irna Tawsen',
+      title: 'Faces of the World',
+      images: [
+        'https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1532635241-17e820acc59f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1630&q=80',
+        'https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80',
+        'https://images.unsplash.com/photo-1604186837056-8e7c2867b6f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      ],
+    },
+    {
+      id:4,
+      name: 'Mark Dale',
+      title: 'Wild Animals',
+      images: [
+        'https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80',
+        'https://images.unsplash.com/photo-1504173010664-32509aeebb62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1654&q=80',
+        'https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+        'https://images.unsplash.com/photo-1531494391841-6ac2ef3859b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1550358864-518f202c02ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      ]
+    }
+  ]
+
+  let currentId;
+  let currentName;
+  let currentTitle;
+  
+  window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    currentId = urlParams.get("id");
+  
+    const currentImage = imageSets.filter(item => item.id == currentId);
+  
+    currentName = currentImage[0].name;
+    currentTitle = currentImage[0].title;
+  
+    const image = `<h1>${currentTitle}</h1>
+        <h2>${currentName}</h2>
+        <img src=${currentImage[0].images[0]} alt=${currentImage[0].name} class="current-image" />
+        <p class="current-image-number">1 of ${currentImage[0].images.length}</p>`;
+    const currentImageContainer = document.querySelector("#current-image-container");
+    currentImageContainer.innerHTML = image;
+  }
+
+// Dark Mode/Light Mode Toggle
+const themeButton = document.querySelector(".theme-button");
+const detailsPageButtons = document.querySelectorAll(".details-page-button");
+const toggleTheme = () => {
+  const body = document.querySelector("body");
+  const heading1 = document.querySelector("h1");
+  const heading2 = document.querySelector("h2");
+  const paragraph = document.querySelectorAll("p");
+
+  if (body.style.backgroundColor === "var(--dark-blue)") {
+    // Loops through the collection "detailsPageButtons" to turn all nodes black for Light Mode
+    for (let i = 0; i < detailsPageButtons.length; i++) {
+        detailsPageButtons[i].style.border = "none";
+    }
+    body.style.backgroundColor = "var(--white)";
+    heading1.style.color = "var(--black)";
+    heading2.style.color = "var(--black)";
+    themeButton.innerHTML = "Dark Mode";
+    // Loops through the collection "paragraph" to turn all nodes black for Light Mode
+    for (let i = 0; i < paragraph.length; i++) {
+      paragraph[i].style.color = "var(--black)";
+    }
+  } else {
+    // Loops through the collection "detailsPageButtons" to turn all nodes white for Dark Mode
+    for (let i = 0; i < detailsPageButtons.length; i++) {
+        detailsPageButtons[i].style.border = "2px solid var(--cyan)";
+    }
+    body.style.backgroundColor = "var(--dark-blue)"
+    heading1.style.color = "var(--white)";
+    heading2.style.color = "var(--white)";
+    themeButton.innerHTML = "Light Mode";
+    // Loops through the collection "paragraph" to turn all nodes white for Dark Mode
+    for (let i = 0; i < paragraph.length; i++) {
+      paragraph[i].style.color = "var(--white)";
+    }
+  }
+}
+// Adds the Dark Mode/Light Mode toggle to the Dark Mode button
+themeButton.onclick = toggleTheme;
