91014b191d005f88e6d0963c7075aef33808dcf9
Nicole Hahn <nicolehahn92@gmail.com>
2023-04-28 20:32:05 -0400

    Minor fix

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -3,5 +3,5 @@ const hamburgerMenuIcon = document.querySelector("#hamburger-menu-icon");
 const openHamburgerMenu = () => {
   const navigationContainer = document.querySelector(".navigation-container");
   navigationContainer.classList.toggle("mobile-menu");
-}
+};
 hamburgerMenuIcon.addEventListener("click", openHamburgerMenu);
